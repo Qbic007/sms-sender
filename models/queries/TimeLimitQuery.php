8fb357e0349c71d11f1d12537aa9c27c1b2d3e32
Serg Pershin <Qbic007@gmail.com>
2018-10-03 00:41:06 +0500

    default scope

--- models/queries/TimeLimitQuery.php
+++ models/queries/TimeLimitQuery.php
@@ -1,10 +1,26 @@
 <?php
 
-
 namespace app\models\queries;
 
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
 
-class TimeLimitQuery
-{
+class TimeLimitQuery extends ActiveQuery {
+    const TIME_LIMIT = 3600; //seconds
 
+    public function init()
+    {
+        /**
+         * @var ActiveRecord $modelClass
+         */
+        $modelClass = $this->modelClass;
+        $tableName = $modelClass::tableName();
+        $this->andWhere(['>=', $tableName.'.status', $this->getLimitDatetime()]);
+        parent::init();
+    }
+
+    protected function getLimitDatetime()
+    {
+        return date('Y.m.d H:i:s', time() - self::TIME_LIMIT);
+    }
 }
