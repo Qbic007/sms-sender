cf928d989781ad4ae6fa2e27a536e3b4f4e0efd5
Serg Pershin <Qbic007@gmail.com>
2018-10-03 23:55:46 +0500

    sender container added

--- config/container.php
+++ config/container.php
@@ -3,6 +3,7 @@
 return [
     'definitions' => [
         \app\interfaces\SmsInterface::class => \app\models\Sms::class,
-        \app\interfaces\WorkerInterface::class => \app\models\Worker::class
+        \app\interfaces\WorkerInterface::class => \app\models\Worker::class,
+        \app\interfaces\SmsSenderInterface::class => \app\models\SmsSender::class
     ]
 ];
