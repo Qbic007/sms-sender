65872827a005697676ee785542bc85a2b4dc1037
Serg Pershin <pershin@exbico.ru>
2018-10-04 13:47:40 +0500

    send function added to worker

--- config/container.php
+++ config/container.php
@@ -2,7 +2,6 @@
 
 return [
     'definitions' => [
-        \app\interfaces\SmsInterface::class => \app\models\Sms::class,
         \app\interfaces\WorkerInterface::class => \app\models\Worker::class
     ]
 ];
--- models/SmsSenderService2.php
+++ models/SmsSenderService2.php
@@ -5,4 +5,5 @@ namespace app\models;
 class SmsSenderService2 extends SmsSender
 {
     protected $url = 'https://sms-service.com/send-sms2';
+    protected $service_id = 2;
 }
